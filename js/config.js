3d3a94b41da5e8a37f0215f1b593d136939cb9b4
Daniele Borba Voigt <51407170+danieleborba@users.noreply.github.com>
2019-06-24 15:57:01 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('select').formSelect();
+  $('.modal').modal();
+  $('.dropdown-trigger').dropdown({inDuration:	1000,
+   belowOrigin: false});
+  $('.tooltipped').tooltip();
+  $('.fixed-action-btn').floatingActionButton();
+  $('.sidenav').sidenav();
+});
+
+
